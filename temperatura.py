e68324c48f994148d1be8c4bb91c07e7e16d72f8
Gabriel <100156732+gameplaybiel@users.noreply.github.com>
2022-08-11 13:08:06 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,9 @@
+#Temperatura de Celsius e Farenheift
+#Declarar as variáveis
+temp_c = print("Temperatura de Celsius")
+#Entrada de dados
+temp_c = float(input("Insira a temperatura em ºC:"))
+#Processamento
+temp_f = (temp_c * 1.8) + 32
+#Apresentar o resultado
+print("A temperatura em Farenheift é:", temp_f,"ºF")
